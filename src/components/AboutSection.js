9f36f69f3e13c832766b6c7fabc447b20833457e
Michael Pearson <michaelpearsun@gmail.com>
2021-02-26 12:29:21 -0600

    AboutSection Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <StyledComponentAbout>
+            <StyledComponentDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledComponentHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledComponentHide>
+                    <StyledComponentHide>
                         <h2>
                             your <span>dreams</span> come 
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledComponentHide>
+                    <StyledComponentHide>
                         <h2>true.</h2>
-                    </div>
+                    </StyledComponentHide>
                 </div>
                 <p>
                     Contact me for any web dev or software engineer ideas that you have. I am open to work on your project today!
                 </p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </StyledComponentDescription>
+            <StyledComponentImage>
                 <img src={home1} alt="guy with a camera" />
-            </div>
-        </div>
+            </StyledComponentImage>
+            </StyledComponentAbout>
     );
 };
+
+const StyledComponentAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-items: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
+const StyledComponentDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight:lighter;
+    }
+`
+
+const StyledComponentImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`
+
+const StyledComponentHide = styled.div`
+    overflow: hidden;
+`
 
 export default AboutSection
