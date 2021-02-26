890a422e78da47da8eab08bb984b3264cb80b5cc
Michael Pearson <michaelpearsun@gmail.com>
2021-02-25 20:21:03 -0600

    Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h2>How Do I Start?</h2>
+                <div className="answer">
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme.</p>
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme Super Caga Fraga Listic Expe</p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>Daily Schedule</h2>
+                <div className="answer">
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme.</p>
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme Super Caga Fraga Listic Expe</p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>Different Payment Methods</h2>
+                <div className="answer">
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme.</p>
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme Super Caga Fraga Listic Expe</p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>What Prodcts do you offer?</h2>
+                <div className="answer">
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme.</p>
+                    <p>Lorem Ipsum Mumbo Jumbo Sumpreme Super Caga Fraga Listic Expe</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
