590e16bb8f31e78ee1d188bd71aed0747d344296
Michael Pearson <michaelpearsun@gmail.com>
2021-03-02 22:50:15 -0600

    Our Work

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+//Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+//Animations
+import { motion } from "framer-motion";
+
+
 
 const OurWork = () => {
     return (
-        <div>
-            <h1>Our Work</h1>
-        </div>
+        <StyledComponentOurWork>
+            <StyledComponentMovie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete" />
+                </Link>
+            </StyledComponentMovie>
+            <StyledComponentMovie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer" />
+                </Link>
+            </StyledComponentMovie>
+            <StyledComponentMovie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes" />
+                </Link>
+            </StyledComponentMovie>
+        </StyledComponentOurWork>
     )
 }
+
+const StyledComponentOurWork = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2{
+        padding: 1rem 0rem;
+    }
+`
+
+const StyledComponentMovie = styled.div`
+    padding-bottom: 10rem;
+    .line{
+        height: 0.5rem;
+        background: #cccccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`
 
 export default OurWork
