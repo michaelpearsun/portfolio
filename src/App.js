4651ea3d596d91b01d4ffb3d4a3077b7ad3a8d8d
Michael Pearson <michaelpearsun@gmail.com>
2021-03-02 22:05:53 -0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
