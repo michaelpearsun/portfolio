5c8514008b6243971db1875bccc92519ba25268e
Michael Pearson <michaelpearsun@gmail.com>
2021-03-05 17:10:05 -0600

    framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,15 +1,27 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import home1 from '../img/home1.png';
-import styled from 'styled-components';
+
 import { StyledComponentAbout, StyledComponentDescription, StyledComponentImage, StyledComponentHide } from '../styles'
 
 const AboutSection = () => {
+
+    const titleAnim = {
+        hidden: {opacity: 0 },
+        show: { opacity: 1, transition: { duration: 2 } },
+    };
+
+    const container = {
+        hidden: { x: 100 },
+        show: { x: 0, transition: { duration: 0.75, ease: "easeOut" } },
+    };
+
     return (
         <StyledComponentAbout>
             <StyledComponentDescription>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <StyledComponentHide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim} initial="hidden" animate="show" >We work to make</motion.h2>
                     </StyledComponentHide>
                     <StyledComponentHide>
                         <h2>
@@ -19,7 +31,7 @@ const AboutSection = () => {
                     <StyledComponentHide>
                         <h2>true.</h2>
                     </StyledComponentHide>
-                </div>
+                </motion.div>
                 <p>
                     Contact me for any web dev or software engineer ideas that you have. I am open to work on your project today!
                 </p>
