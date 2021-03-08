1c1f089377717ddbcbce2bd02b3fed04240b7b1c
Michael Pearson <michaelpearsun@gmail.com>
2021-03-07 19:23:45 -0600

    Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+import styled from 'styled-components';
 
 const ContactUs = () => {
     return (
-        <motion.div variants={ pageAnimation } exit="exit" initial="hidden" animate="show" style={{ background: "#fff" }}>
-            <h1>Contact Us</h1>
-        </motion.div>
+        <StyledComponentContact variants={pageAnimation} exit="exit" initial="hidden" animate="show" style={{ background: "#fff" }}>
+            <StyledComponentTitle>
+                <StyledComponentHide>
+                    <motion.h2 variants={titleAnim} >Get in touch.</motion.h2>
+                </StyledComponentHide>
+            </StyledComponentTitle>
+            <div>
+                <StyledComponentHide>
+                    <StyledComponentSocial variants={titleAnim}>
+                        <StyledComponentCircle />
+                        <h2>Send Us A Message</h2>
+                    </StyledComponentSocial>
+                </StyledComponentHide>
+                <StyledComponentHide>
+                    <StyledComponentSocial variants={titleAnim}>
+                        <StyledComponentCircle />
+                        <h2>SSend an email</h2>
+                    </StyledComponentSocial>
+                </StyledComponentHide>
+                <StyledComponentHide>
+                    <StyledComponentSocial variants={titleAnim}>
+                        <StyledComponentCircle />
+                        <h2>Social Media</h2>
+                    </StyledComponentSocial>
+                </StyledComponentHide>
+            </div>
+        </StyledComponentContact>
     )
-}
+};
 
-export default ContactUs
+const StyledComponentContact = styled(motion.div)`
+    padding: 5rem 10rem;
+    color: #353535;
+    min-height: 90vh;
+`;
+
+const StyledComponentTitle = styled.div`
+    margin-bottom: 4rem;
+    color: black;
+`;
+
+const StyledComponentHide = styled.div`
+    overflow: hidden;
+`;
+
+const StyledComponentCircle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #353535;
+`;
+
+const StyledComponentSocial = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+        margin: 2rem;
+    }
+`
+
+export default ContactUs;
